cd17af6a877195a6f9614672f3e38a659d6d2a72
autopp <autopp.inc@gmail.com>
2020-11-11 00:28:30 +0900

    use type parameter

--- tests/service.test.ts
+++ tests/service.test.ts
@@ -1,12 +1,15 @@
 import { Calc, Dist } from "../src/service"
 
+function typedMock<F extends (...args: any) => any>() {
+  return jest.fn<ReturnType<F>, Parameters<F>>()
+}
+
 describe("Dist.distance", () => {
   it("calculate euclidean distance", () => {
     const calc: Calc = {
-      pow: jest.fn().mockImplementation((x, y) => Math.pow(x, y)),
-      sqrt: jest.fn().mockImplementation((x) => Math.sqrt(x))
+      pow: jest.fn<number, [number, number]>().mockImplementation((x, y) => Math.pow(x, y)),
+      sqrt: typedMock<Calc["sqrt"]>().mockImplementation((x) => Math.sqrt(x))
     }
     const dist = new Dist(calc)
-    expect(dist)
   })
 })
